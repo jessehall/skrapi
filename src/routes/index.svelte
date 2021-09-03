2b41774b9dad174f585b35f903b190b1fb5e094c
test <test@test.com>
2021-09-03 15:55:55 -0700

    testing changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <section class="container">
-  <h2>Skrapi</h2>
+  <h2>Testing Krapi</h2>
   <p>
     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, magnam at totam cumque quo ipsum consequatur rerum voluptatum ullam corporis dolore, quas sint aliquid accusamus eveniet veritatis ad iste dolor?
   </p>
